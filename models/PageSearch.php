1be58fca2dbe6984aa9bd143dc74d215f4de72e8
aleksandr <lex@email.com.ua>
2015-09-20 14:37:21 +0300

    Generate test model Comment.php, File.php, Page.php, PageSearch.php

---
+++ models/PageSearch.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+use yii\base\Model;
+use yii\data\ActiveDataProvider;
+use app\models\Page;
+
+/**
+ * PageSearch represents the model behind the search form about `app\models\Page`.
+ */
+class PageSearch extends Page
+{
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['id', 'user_id', 'live'], 'integer'],
+            [['title', 'content', 'date_entered', 'date_updated'], 'safe'],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function scenarios()
+    {
+        // bypass scenarios() implementation in the parent class
+        return Model::scenarios();
+    }
+
+    /**
+     * Creates data provider instance with search query applied
+     *
+     * @param array $params
+     *
+     * @return ActiveDataProvider
+     */
+    public function search($params)
+    {
+        $query = Page::find();
+
+        $dataProvider = new ActiveDataProvider([
+            'query' => $query,
+        ]);
+
+        $this->load($params);
+
+        if (!$this->validate()) {
+            // uncomment the following line if you do not want to return any records when validation fails
+            // $query->where('0=1');
+            return $dataProvider;
+        }
+
+        $query->andFilterWhere([
+            'id' => $this->id,
+            'user_id' => $this->user_id,
+            'live' => $this->live,
+            'date_entered' => $this->date_entered,
+            'date_updated' => $this->date_updated,
+        ]);
+
+        $query->andFilterWhere(['like', 'title', $this->title])
+            ->andFilterWhere(['like', 'content', $this->content]);
+
+        return $dataProvider;
+    }
+}
