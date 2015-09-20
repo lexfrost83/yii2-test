9f5237acfbfafd3d1ded8469f63eb0d172ef53b5
aleksandr <lex@email.com.ua>
2015-09-20 14:16:52 +0300

    Config connect to db

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+    'dsn' => 'mysql:host=localhost;dbname=yiibook2_cms',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
